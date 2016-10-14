57c0ec37f906d01422f0c4bb944e09c6f9913963
capo2183 <howard19891013@gmail.com>
2016-10-15 03:12:43 +0800

    Create Project

---
+++ Assets/CH3/Editor/CameraFollowerEditor.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(CameraFollow))]
+public class CameraFollowerEditor : Editor
+{
+    CameraFollow m_Target;
+
+    private bool _isTrackingXAxis;
+    
+    public override void OnInspectorGUI()
+    {
+        m_Target = (CameraFollow)target;
+
+        // Toggle(標題, 預設值)，勾選框元件
+        _isTrackingXAxis = EditorGUILayout.Toggle("Tracking X Axis", _isTrackingXAxis);
+
+        // 從 BeginDisabledGroup(Boolean) 到 EndDisabledGroup() 中間的範圍是否可以被選取
+        // 取決於 BeginDisabledGroup 傳入的布林參數
+        EditorGUI.BeginDisabledGroup(_isTrackingXAxis == false);
+
+        // FloatField(標題, 預設值)，浮點數輸入元件
+        // 原本的目標物件(Camera)裡的變數都要設定為 Inspector 欄位中修改的數值
+        m_Target.xMargin = EditorGUILayout.FloatField("Margin", m_Target.xMargin);
+        m_Target.xSmooth = EditorGUILayout.FloatField("Smooth", m_Target.xSmooth);
+        m_Target.minMaxX.x = EditorGUILayout.FloatField("Min position", m_Target.minMaxX.x);
+        m_Target.minMaxX.y = EditorGUILayout.FloatField("Max positio", m_Target.minMaxX.y);
+
+        // 我們要用 Slider 來控制攝影機的位置，在此要先取得目標物件(Camera)的 Position
+        Vector3 cameraPosition = m_Target.transform.position;
+        // Slider(標題, 預設值, 最小值, 最大值)，滑桿元件
+        cameraPosition.x = EditorGUILayout.Slider("Camera X Position", cameraPosition.x, m_Target.minMaxX.x, m_Target.minMaxX.y);
+        EditorGUI.EndDisabledGroup();
+
+        if (cameraPosition.x != m_Target.transform.position.x)
+        {
+            // 在修改目標物件(Camera)的位移前，先記錄到 Undo List 中。開發者可以藉由 Undo 的功能回到 Transform 尚未位移前的狀態
+            Undo.RecordObject(m_Target.transform, "Change Camera X Position");
+            // 原本的目標物件(Camera)裡的 position 都要設定為 Inspector 滑桿中修改的數值
+            m_Target.transform.position = cameraPosition;
+        }
+    }
+}
---
+++ Assets/CH3/Scripts/CameraFollow.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // X 軸方向的追蹤
+    public bool isTrackingXAxis; // 是否開啟X 軸方向的追蹤
+    public float xMargin = 1f;   // 角色離攝影機中心多遠以上會開始追蹤
+    public float xSmooth = 8f;   // 攝影機追蹤的速度                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+    public Vector2 minMaxX;      // 攝影機追蹤的最小最大值 (最小值, 最大值)
+    public float xPos;           // 攝影機實際的 X 位置
+
+    // Y 軸方向的追蹤
+    public bool isTrackingYAxis;
+    public float yMargin = 1f;
+    public float ySmooth = 8f;
+    public Vector2 minMaxY;
+    public float yPos;
+
+    private Transform m_Player; // 玩家角色的 Transform
+
+    private void Awake()
+    {
+        m_Player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // 檢查位置 X 是否超出範圍
+    private bool CheckXMargin()
+    {
+        return Mathf.Abs(transform.position.x - m_Player.position.x) > xMargin;
+    }
+
+    // 檢查位置 Y 是否超出範圍
+    private bool CheckYMargin()
+    {
+        return Mathf.Abs(transform.position.y - m_Player.position.y) > yMargin;
+    }
+
+
+    private void Update()
+    {
+        TrackPlayer();
+    }
+
+    private void TrackPlayer()
+    {
+        float targetX = transform.position.x;
+        float targetY = transform.position.y;
+
+        if (CheckXMargin())
+            targetX = Mathf.Lerp(transform.position.x, m_Player.position.x, xSmooth * Time.deltaTime);
+
+        if (CheckYMargin())
+            targetY = Mathf.Lerp(transform.position.y, m_Player.position.y, ySmooth * Time.deltaTime);
+
+        // 攝影機的位置必須要在最小和最大值之間
+        targetX = Mathf.Clamp(targetX, minMaxX.x, minMaxX.y);
+        targetY = Mathf.Clamp(targetY, minMaxY.x, minMaxY.y);
+
+        // 設定攝影機位置
+        transform.position = new Vector3(targetX, targetY, transform.position.z);
+    }
+}
