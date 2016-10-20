382e8787fd19f8988fcc313db62e010c9931eae9
yuhao <11@11.11>
2016-10-20 20:31:27 +0800

    Finish BlockControllerEditor

--- Assets/CH3/Editor/BlockControllerEditor.cs
+++ Assets/CH3/Editor/BlockControllerEditor.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using System.Collections;
+using System.Reflection;
 
 [CustomEditor(typeof(BlockController))]
 public class BlockControllerEditor : Editor {
@@ -24,30 +25,47 @@ public class BlockControllerEditor : Editor {
             DrawType(i);
         }
 
-        // DrawAddTypeButton();
+        DrawAddTypeButton();
     }
 
     void DrawType(int index)
     {
         if (index < 0 || index >= m_Target.Types.Count)
             return;
-
+        
         GUILayout.BeginHorizontal();
         {
             GUILayout.Label("Name", EditorStyles.label, GUILayout.Width(50));
+
+            // BeginChangeCheck() 用來檢查在 BeginChangeCheck() 和 EndChangeCheck() 之間是否有 Inspector 變數改變
+            EditorGUI.BeginChangeCheck();
             string newName = GUILayout.TextField(m_Target.Types[index].Name, GUILayout.Width(120));
             Color newColor = EditorGUILayout.ColorField(m_Target.Types[index].HitColor);
-
+            
             m_Target.Types[index].Name = newName;
             m_Target.Types[index].HitColor = newColor;
 
+            // 如果 Inspector 變數有改變，EndChangeCheck() 會回傳 True，才有必要去做變數存取
+            if (EditorGUI.EndChangeCheck())
+            {
+                // 在修改之前建立 Undo/Redo 記錄步驟
+                Undo.RecordObject(m_Target.transform.gameObject, "Modify Types");
+                Debug.LogError("[RecordObject] Modify Types");
+
+                m_Target.Types[index].Name = newName;
+                m_Target.Types[index].HitColor = newColor;
+
+                // 每當直接修改 Inspector 變數，而不是使用 serializedObject 修改時，必須要告訴 Unity 這個 Compoent 已經修改過了
+                // 在下一次存檔時，必須要儲存這個變數
+                EditorUtility.SetDirty(m_Target);
+            }
+
             if (GUILayout.Button("Remove"))
             {
+                // 系統會 "登" 一聲
                 EditorApplication.Beep();
-
-                //DisplayDialog is a very useful method to create a simple popup check. You can use this to simply display information to the user 
-                //that something has changed which the user has to confirm by clicking OK, or you can even ask simple Yes, No questions to, 
-                //like in this example, ask if the user really wants to delete something
+                                
+                // 顯示對話框功能(帶有 OK 和 Cancel 兩個按鈕)
                 if (EditorUtility.DisplayDialog("Really?", "Do you really want to remove the state '" + m_Target.Types[index].Name + "'?", "Yes", "No") == true)
                 {
                     m_Target.Types.RemoveAt(index);
@@ -57,5 +75,20 @@ public class BlockControllerEditor : Editor {
             }
         }
         GUILayout.EndHorizontal();
+
+        Undo.FlushUndoRecordObjects();
+    }
+
+    void DrawAddTypeButton()
+    {
+        if (GUILayout.Button("Add new State", GUILayout.Height(30)))
+        {
+            Undo.RecordObject(m_Target, "Add new Type");
+
+            Debug.LogError("[RecordObject] Add new Type");
+
+            m_Target.Types.Add(new BrickType { Name = "New State" });
+            EditorUtility.SetDirty(m_Target);
+        }
     }
 }
